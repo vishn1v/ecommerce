ba987e8033de71fd92d980330f6e89547a713db1
Bhardwaj <bhardwaj@Bhardwajs-Air.lan>
2024-02-04 20:28:34 -0500

    updated bg images for each screen

--- Main.py
+++ Main.py
@@ -18,6 +18,11 @@ flipzondb = sql.connect(
 create_userdetails_table = """
 CREATE TABLE IF NOT EXISTS user_details (  
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phone VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     username VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 )
@@ -38,7 +43,7 @@ class flipzon:
         self.root.attributes('-alpha', 1)
         #self.image_tk = None
 
-        img = Image.open("/Users/bhardwaj/Documents/pictures for project/_defd105c-4bb3-46e3-826a-562cc387d98e.jpeg")
+        img = Image.open("/Users/bhardwaj/Documents/pictures for project/PhotoGrid_Plus_1707021941021.png")
         resized_image = img.resize((1035, 850), Image.LANCZOS)
         image_tk = ImageTk.PhotoImage(resized_image)
 
@@ -56,32 +61,32 @@ class flipzon:
         self.background_label = tk.Label(root)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.login_frame=tk.Frame(root, bg="grey")
+        self.login_frame=tk.Frame(root, bg="white")
         #login_frame.place(x=0,y=0,relheight=1, relwidth=1)
         #login_frame.attributes('-alpha', 0.9)
-        self.login_label = tk.Label(self.login_frame, text="provide your login details",bg="grey",font=("Calibri","20","bold"))
-        self.login_label.grid(row=0,column=0,columnspan=5,pady=10)
-        self.login_username_label= tk.Label(self.login_frame, text="username",bg="grey",fg="azure",font=("Calibri","15","bold"),pady=5)
+        self.login_label = tk.Label(self.login_frame, text="Provide Your Login Details",bg="white",fg="black",font=("Calibri","20"))
+        self.login_label.grid(row=0,column=2,columnspan=5,pady=10)
+        self.login_username_label= tk.Label(self.login_frame, text="Username",bg="white",fg="black",font=("Calibri","15","bold"),pady=5)
         self.login_username_entry= tk.Entry(self.login_frame)
-        self.login_password_label= tk.Label(self.login_frame, text="password",bg="grey",font=("Calibri","15","bold"))
+        self.login_password_label= tk.Label(self.login_frame, text="Password",bg="white",fg="black",font=("Calibri","15","bold"))
         self.login_password_entry= tk.Entry(self.login_frame)
-        self.login_button = Button(self.login_frame, text="Login",bg="#FFFF00",fg="black",command=self.login,highlightbackground="grey",borderless=1)
-        self.forgot_password_button = Button(self.login_frame, text="Forgot password?",bg="gold",fg="black",command=self.show_forgot_password_screen,highlightbackground="grey",borderless=1)
-        self.createacc_button=tk.Button(self.login_frame, text="New User? Create Your account",bg="yellow",fg="black",command=self.show_create_account_screen,highlightbackground="grey")
+        self.login_button = Button(self.login_frame, text="Login",bg="sky blue",fg="black",command=self.login,highlightbackground="grey",borderless=1)
+        self.forgot_password_button = Button(self.login_frame, text="Forgot password?",bg="light green",fg="black",command=self.show_forgot_password_screen,highlightbackground="grey",borderless=1)
+        self.createacc_button= Button(self.login_frame, text="New User? Create Your account",bg="light green",fg="black",command=self.show_create_account_screen,highlightbackground="grey",borderless=1)
 
-        self.login_username_label.grid(row=1,column=0)
-        self.login_username_entry.grid(row=1, column=1)
-        self.login_password_label.grid(row=2,column=0)
-        self.login_password_entry.grid(row=2,column=1)
-        self.login_button.grid(row=3,column=0,columnspan=2,pady="10")
-        self.forgot_password_button.grid(row=4,column=0,columnspan=3)
-        self.createacc_button.grid(row=6,column=0,columnspan=3)
+        self.login_username_label.grid(row=1,column=1)
+        self.login_username_entry.grid(row=1, column=2)
+        self.login_password_label.grid(row=2,column=1)
+        self.login_password_entry.grid(row=2,column=2)
+        self.login_button.grid(row=3,column=2,columnspan=2,pady="10")
+        self.forgot_password_button.grid(row=4,column=2,columnspan=3)
+        self.createacc_button.grid(row=7,column=2,columnspan=3)
         #self.l_frame()
         #self.destroy_background_image()
         # self.create_account_frame.pack_forget()
         # self.forgot_password_frame.pack_forget()
-        self.login_frame.place(x=327,y=370,width=390)
-        self.update_background_image("/Users/bhardwaj/Documents/pictures for project/_defd105c-4bb3-46e3-826a-562cc387d98e.jpeg")
+        self.login_frame.place(x=327,y=230,width=390)
+        self.update_background_image("/Users/bhardwaj/Documents/pictures for project/PhotoGrid_Plus_1707021941021.png")
         #self.login_frame.pack() 
 
     def show_create_account_screen(self):
@@ -91,25 +96,47 @@ class flipzon:
 
         self.background_label = tk.Label(root)
         self.background_label.place(x=0, y=0, relwidth=1, relheight=1)
-        self.create_account_frame=tk.Frame(root,bg="grey")
-        self.create_label = tk.Label(self.create_account_frame, text="create your account here",bg="grey",font=("Calibri","20","bold"))
-        self.create_label.grid(row=0,column=0,columnspan=5,pady=10)
-        self.create_username_label= tk.Label(self.create_account_frame, text="username",bg="grey",fg="azure",font=("Calibri","15","bold"),pady="5")
+        self.create_account_frame=tk.Frame(root,bg="black")
+        self.create_label = tk.Label(self.create_account_frame, text="Create your account here",font=("Helvetica",20,"bold"),bg="black",fg="white")
+        self.create_label.grid(row=0,column=1,padx=5,pady=10,sticky="w")
+        self.create_firstname_label= tk.Label(self.create_account_frame, text="First Name",bg="black",fg="azure",font=("Calibri","15","bold"),pady="5")
+        self.create_firstname_entry= tk.Entry(self.create_account_frame)
+        self.create_lastname_label= tk.Label(self.create_account_frame, text="Last Name",bg="black",font=("Calibri","15","bold"),pady="5")
+        self.create_lastname_entry= tk.Entry(self.create_account_frame)
+        self.create_phone_label= tk.Label(self.create_account_frame, text="Phone",bg="black",font=("Calibri","15","bold"),pady="5")
+        self.create_phone_entry= tk.Entry(self.create_account_frame)
+        self.crreate_address_label= tk.Label(self.create_account_frame, text="Address",bg="black",font=("Calibri","15","bold"),pady="5")
+        self.create_address_entry= tk.Entry(self.create_account_frame)
+        self.create_email_label= tk.Label(self.create_account_frame, text="Email",bg="black",font=("Calibri","15","bold"),pady="5")
+        self.create_email_entry= tk.Entry(self.create_account_frame)
+        self.create_username_label= tk.Label(self.create_account_frame, text="username",bg="black",fg="azure",font=("Calibri","15","bold"),pady="5")
         self.create_username_entry= tk.Entry(self.create_account_frame)
-        self.create_password_label= tk.Label(self.create_account_frame, text="password",bg="grey",font=("Calibri","15","bold"),pady="5")
+        self.create_password_label= tk.Label(self.create_account_frame, text="password",bg="black",font=("Calibri","15","bold"),pady="5")
         self.create_password_entry= tk.Entry(self.create_account_frame)
-        self.create_button = tk.Button(self.create_account_frame, text="Create",bg="yellow",command=self.create_account,highlightbackground="grey")
-        self.create_back_login_button = tk.Button(self.create_account_frame, text="Back to login",bg="gold",command=self.show_login,highlightbackground="grey")
+        self.create_button = Button(self.create_account_frame, text="Create",bg="yellow",command=self.create_account,highlightbackground="black",borderless=1)
+        self.create_back_login_button = Button(self.create_account_frame, text="Back to login",bg="yellow",command=self.show_login,highlightbackground="black",borderless=1)
 
-        self.create_username_label.grid(row=1,column=0)
-        self.create_username_entry.grid(row=1, column=1)
-        self.create_password_label.grid(row=2,column=0)
-        self.create_password_entry.grid(row=2,column=1)
-        self.create_button.grid(row=3,column=0,columnspan=2,pady="10")
-        self.create_back_login_button.grid(row=4,column=0,columnspan=3)
-        self.create_account_frame.pack()
+        self.create_firstname_label.grid(row=1,column=0)
+        self.create_firstname_entry.grid(row=1, column=1)
+        self.create_lastname_label.grid(row=2,column=0)
+        self.create_lastname_entry.grid(row=2,column=1)
+        self.create_phone_label.grid(row=3,column=0)
+        self.create_phone_entry.grid(row=3,column=1)
+        self.crreate_address_label.grid(row=4,column=0)
+        self.create_address_entry.grid(row=4,column=1)
+        self.create_email_label.grid(row=5,column=0)
+        self.create_email_entry.grid(row=5,column=1)
+        self.create_username_label.grid(row=6,column=0)
+        self.create_username_entry.grid(row=6, column=1)
+        self.create_password_label.grid(row=7,column=0)
+        self.create_password_entry.grid(row=7,column=1)
+        self.create_button.grid(row=9,column=0,columnspan=2,pady="10")
+        self.create_back_login_button.grid(row=10,column=0,columnspan=3)
+        self.create_account_frame.place(x=230,y=150,height=700,width=550)
+        # self.frame1=Frame(self.app,width=700,height=450,bg="white")
+        # self.frame1.place(x=180,y=90)
 
-        self.update_background_image("/Users/bhardwaj/Documents/pictures for project/force-majeure-du8AbwM5z2g-unsplash.jpg")
+        self.update_background_image("/Users/bhardwaj/Documents/pictures for project/PhotoGrid_Plus_1707024804464.png")
 
     def show_forgot_password_screen(self):
         #self.destroy_background_image()
@@ -145,6 +172,11 @@ class flipzon:
 
 #Function to create account
     def create_account(self):
+        c_firstname=self.create_firstname_entry.get()
+        c_lastname=self.create_lastname_entry.get()
+        c_phone=self.create_phone_entry.get()
+        c_address=self.create_address_entry.get()
+        c_email=self.create_email_entry.get()
         c_username=self.create_username_entry.get()
         c_password=self.create_password_entry.get()
 
@@ -156,7 +188,7 @@ class flipzon:
             #cursor.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR(255), password VARCHAR(255))")
 
             # Insert data into the table
-            cursor.execute("INSERT INTO user_details (username, password) VALUES (%s, %s)", (c_username, c_password))
+            cursor.execute("INSERT INTO user_details (first_name, last_name,phone,address,email,username, password) VALUES (%s, %s, %s, %s, %s, %s)", (c_firstname,c_lastname,c_phone,c_address,c_email,c_username, c_password))
 
         
             flipzondb.commit()
